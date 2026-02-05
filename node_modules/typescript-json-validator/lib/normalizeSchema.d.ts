1d196616f47152d5b5184ee63a15c9d5867951f1
Marco <marco.pontone@gmail.com>
2026-02-05 15:17:05 +0100

    Delete node_modules directory

--- node_modules/typescript-json-validator/lib/normalizeSchema.d.ts
+++
@@ -1,2 +0,0 @@
-import * as TJS from 'typescript-json-schema';
-export default function normalizeSchema(schema: TJS.Definition): TJS.Definition;
--- node_modules/via-keyboards/bin/cli.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-var tsNode = require('ts-node');
-tsNode.register({
-  project: './node_modules/via-keyboards/tsconfig.json',
-});
-require('../scripts/build-all');
